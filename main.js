a31938c5ff686fd1bc130c40e4893b93ea3a13e2
Rosemeire Ferreira da Silva <rosemereh2018@gmail.com>
2021-07-07 00:26:06 -0300

    curso html e javascript horario

---
+++ main.js
@@ -0,0 +1,24 @@
+function load() {
+    var d = new Date()
+    var min = d.getMinutes()
+    var hora = d.getHours()
+
+    document.getElementById("timeText").innerHTML = `Agora são ${hora}:${min}`
+
+    if(hora >= 01 && hora <= 12) {
+    document.getElementById("image").src = "img/pexels-todd-trapani-1420440.jpg"
+    document.body.style.backgroundImage = "linear-gradient(to right, #F5C515, #C59C03"
+
+    } else if (hora > 12 && hora < 18) {
+    document.getElementById("image").src = "img/pexels-hristo-sahatchiev-821357.jpg"
+    document.body.style.backgroundImage = "linear-gradient(to right, #A9D0F0, #A1B4C4"
+
+    } else {
+        document.getElementById("image").src = "img/pexels-pixabay-355465.jpg"
+        document.body.style.backgroundImage = "linear-gradient(to right, #065393, #01203A"
+    }
+}
+
+// muito top...
+//maravilhoso jascript!!!
+
